7a7c18b35aa4301281fb3c93ecfce3eb981e0020
Marcia Cruz <marcia.vandinha@gmail.com>
2023-01-23 06:36:20 -0400

    Mentoria e Curso

---
+++ desafio_dio_POO/src/br/com/dio/desafio/dominio/Main.java
@@ -0,0 +1,30 @@
+package br.com.dio.desafio.dominio;
+
+import java.time.LocalDate;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Curso curso1 = new Curso();
+		curso1.setTitulo("curso java");
+		curso1.setDescricao("descrição curso java");
+		curso1.setCargaHoraria(8);
+		
+		Curso curso2 = new Curso();
+		curso2.setTitulo("curso javascript");
+		curso2.setDescricao("descrição curso js");
+		curso2.setCargaHoraria(4);
+		
+		System.out.println(curso1);
+		System.out.println(curso2);
+		
+		Mentoria mentoria = new Mentoria();
+		mentoria.setTitulo("mentoria de java");
+		mentoria.setDescricao("descrição mentoria java");
+		mentoria.setData(LocalDate.now());
+		
+		System.out.println(mentoria);
+	}
+
+}
